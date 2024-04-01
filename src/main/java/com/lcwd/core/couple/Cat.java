8456118d91fb64fd6819a309ac27f243ce6f794b
Sarvesh Dutt Richhariya <sarveshdutt40@gmail.com>
2024-04-01 17:55:01 +0530

    @ComponentScan and ApplicationContext and GettingBean

--- src/main/java/com/lcwd/core/couple/Cat.java
+++ src/main/java/com/lcwd/core/couple/Cat.java
@@ -2,7 +2,7 @@ package com.lcwd.core.couple;
 
 import org.springframework.stereotype.Component;
 
-@Component
+
 public class Cat implements Animal{
     public void play(){
         System.out.println("cat is playing");
